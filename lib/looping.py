febebf34b686955b0ed5868a8f041277fb5fa012
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-05-22 10:10:33 -0400

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
+        
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_integers = []
+    for x in int_list:
+        square_integers.append( x * x)
+    return square_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+            i += 1
+        elif i % 3 == 0:
+            print("Fizz")
+            i += 1
+        elif i % 5 == 0:
+            print("Buzz")
+            i += 1
+        else:
+            print(f"{i}")
+            i += 1
